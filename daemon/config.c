5303bbff42aecd639b5315ba8967eda5c27022b8
cbdev <cb@cbcdn.com>
2017-12-02 18:55:48 +0100

    makefile clean target, command structures

--- daemon/config.c
+++ daemon/config.c
@@ -1,4 +1,20 @@
-int config_parse(char* file){
-	//TODO implement
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "config.h"
+
+int config_parse(char* cfg_file){
+	size_t line_size = 0;
+	ssize_t status;
+	char* line = NULL;
+
+	FILE* source = fopen(cfg_file, "r");
+	if(!source){
+		fprintf(stderr, "Failed to open configuration file %s: %s\n", cfg_file, strerror(errno));
+		return 1;
+	}
+
 	return 0;
 }
